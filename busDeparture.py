5737a6d4c45676ec46ab03c1cafdbc4bc9f9ca96
Bodin Bizetic <dinbo99@gmail.com>
2019-10-10 23:13:14 +0200

    Added reverse orientation for buses

--- busDeparture.py
+++ busDeparture.py
@@ -1,6 +1,7 @@
 """
 /brief Program gets next departure of some bus from official site of "GSP"
 """
+#Idea for improvment: get links to buses automaticaly
 
 import requests
 from bs4 import BeautifulSoup
@@ -13,11 +14,21 @@ class BusStart:
     """
     def __init__(self, url="https://www.busevi.com/red-voznje/linija-eko-1-vukov-spomenik-naselje-belville/"):
         page = requests.get(url)
+        self.url = url
         self.soup = BeautifulSoup(page.text, 'html.parser')
         self.minutes = []
         self.hour = 0
         self.getNext()
-          
+
+    def getReverseStation(self):
+        """
+        /brief Function that returns link for different orientation of bus
+        /returns url link as string
+        """
+        links = self.soup.find_all(class_="vc_tta-tab")
+        link = links[1].find('a')
+        return self.url + link['href']
+
     def getNext(self):
         """
         /brief Function that scraps information about next departure of Bus
@@ -35,7 +46,7 @@ class BusStart:
 
     def departureTime(self):
         """
-        /brief Funtion that is used to create list of all times
+        /brief Funtion that is used to create list of all timestamps of buses departure
         /return Returns list of times in format hour:minutes
         """
         temp = []
@@ -47,9 +58,8 @@ class BusStart:
 
 def main():
     e = BusStart()
-    temp = e.departureTime()
-    for i in temp:
-        print(i)
+    temp = e.getReverseStation()
+    print(temp)
 
 
 if __name__ == "__main__":
